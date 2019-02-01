63deb4a520c40397fed37734813d93079775fd2d
dullme <admin@dullme.com>
2019-02-01 13:03:12 +0800

    完成后台会员卡批量充值

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -105,7 +105,10 @@ class UserController extends Controller
         $grid->bank_card_number('银行卡');
         $grid->bank('开户行');
         $grid->amount('余额');
-        $grid->taskHistories('当日收益')->sum('amount');
+        $grid->taskHistories('当日收益')->display(function ($taskHistories){
+            
+            return round($taskHistories->sum('amount'), 2);
+        });
         $grid->one_day_max_send_count('当日最大发送数');
         $grid->mode('防封模式')->switch([
             'on' => ['text' => '开启'],
