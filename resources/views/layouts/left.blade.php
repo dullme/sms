915a5ad30a3c53b9073ac2c286de7626e0889d88
JINJIALEI <admin@dullme.com>
2019-03-10 22:18:43 +0800

    新增统计卡的信息

--- resources/views/layouts/left.blade.php
+++ resources/views/layouts/left.blade.php
@@ -1,6 +1,6 @@
 <div id="menu" style="max-width: 100px; padding-top: 10px; border-right: 2px solid rgb(187, 187, 187)">
     <a href="{{ route('info_edit') }}" class="btn btn-default top-btn" style="width: 98px; font-size: 14px; margin-right: 0">信息修改</a>
     <a href="{{ route('info_withdraw') }}" class="btn btn-default top-btn" style="width: 98px; font-size: 14px; margin-right: 0">账户提现</a>
-    <a href="{{ route('info_transfer') }}" class="btn btn-default top-btn" style="width: 98px; font-size: 14px; margin-right: 0">内部转账</a>
+    {{--<a href="{{ route('info_transfer') }}" class="btn btn-default top-btn" style="width: 98px; font-size: 14px; margin-right: 0">内部转账</a>--}}
     <a href="{{ route('info_transaction') }}" class="btn btn-default top-btn" style="width: 98px; font-size: 14px; margin-right: 0">交易查询</a>
 </div>
